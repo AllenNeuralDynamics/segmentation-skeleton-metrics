b2d6160db15ac5d8235ea576989d11640b189f32
anna-grim <anna.grim@alleninstitute.org>
2023-01-10 12:08:17 -0800

    updates suggested by linters

--- resources/demo.py
+++ resources/demo.py
@@ -5,7 +5,6 @@ Created on Tue Jan 3 15:00:00 2023
 @email: anna.grim@alleninstitute.org
 
 """
-import os
 
 from aind_segmentation_evaluation.merge_metric import MergeMetric
 from aind_segmentation_evaluation.split_metric import SplitMetric
@@ -14,9 +13,7 @@ if __name__ == "__main__":
 
     # Initializations
     shape = (148, 226, 282)
-    output_dir = (
-        "path to directory where outputs are written to"
-    )
+    output_dir = "path to directory where outputs are written to"
     path_to_pred_volume = "path to predicted volumetric segmentation"
     target_graphs_dir = "path to directory target swc files"
 
