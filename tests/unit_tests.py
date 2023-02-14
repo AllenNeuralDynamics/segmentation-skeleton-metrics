ce7bbe45bcbb0c942a2894655975974be53e0378
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2023-02-14 13:25:40 -0800

    linters

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,13 +1,12 @@
-import os
+"""unit tests"""
 import numpy as np
-import aind_segmentation_evaluation.graph_routines as gr
 import aind_segmentation_evaluation.split_metric as sp
 import aind_segmentation_evaluation.merge_metric as me
 import networkx as nx
-from numpy.random import random
 
 
 def make_graph(num_nodes=10):
+    """creates graph"""
     graph = nx.Graph()
     for i in range(num_nodes):
         graph.add_node(i, xyz=(i, i, 0), idx=(i, i, 0))
@@ -15,13 +14,17 @@ def make_graph(num_nodes=10):
             graph.add_edge(i - 1, i)
     return graph
 
-def make_volume(shape=(10,10,10)):
+
+def make_volume(shape=(10, 10, 10)):
+    """creates volume"""
     volume = np.zeros(shape, dtype=np.uint)
     for i in range(shape[0]):
         volume[i, i, 0] = 1
     return volume
 
+
 def test_break(graph, volume):
+    """tests break"""
     corrupt_volume = volume.copy()
     corrupt_volume[0, 0, 0] = 0
     corrupt_volume[1, 1, 0] = 0
@@ -33,10 +36,12 @@ def test_break(graph, volume):
     split_metric.detect_mistakes()
     test1 = split_metric.site_cnt == 0
     test2 = split_metric.edge_cnt == 2
-    result = 'Pass' if test1 and test2 else 'Fail'
+    result = "Pass" if test1 and test2 else "Fail"
     return result
 
+
 def test_simple_split(graph, volume):
+    """tests simple split method"""
     split_volume = make_split(volume)
     split_metric = sp.SplitMetric(
         shape,
@@ -46,10 +51,12 @@ def test_simple_split(graph, volume):
     split_metric.detect_mistakes()
     test1 = split_metric.site_cnt == 1
     test2 = split_metric.edge_cnt == 1
-    result = 'Pass' if test1 and test2 else 'Fail'
+    result = "Pass" if test1 and test2 else "Fail"
     return result
 
+
 def test_complex_split(graph, volume):
+    """tests complex split method"""
     split_volume = make_split(volume, num_split_edges=3)
     split_metric = sp.SplitMetric(
         shape,
@@ -59,40 +66,41 @@ def test_complex_split(graph, volume):
     split_metric.detect_mistakes()
     test1 = split_metric.site_cnt == 1
     test2 = split_metric.edge_cnt == 3
-    result = 'Pass' if test1 and test2 else 'Fail'
+    result = "Pass" if test1 and test2 else "Fail"
     return result
 
+
 def test_simple_merge(graph, volume):
+    """tests simple merge"""
     split_volume = make_split(volume)
     merge_metric = me.MergeMetric(
-        shape,
-        pred_graphs=graph,
-        target_volume=split_volume
+        shape, pred_graphs=graph, target_volume=split_volume
     )
     merge_metric.detect_mistakes()
     test1 = merge_metric.site_cnt == 1
     test2 = merge_metric.edge_cnt == 5
-    result = 'Pass' if test1 and test2 else 'Fail'
+    result = "Pass" if test1 and test2 else "Fail"
     return result
 
+
 def test_complex_merge(graph, volume):
+    """tests complex merge"""
     split_volume = make_split(volume, num_split_edges=2)
-    print(split_volume[:,:,0])
+    print(split_volume[:, :, 0])
     merge_metric = me.MergeMetric(
-        shape,
-        pred_graphs=graph,
-        target_volume=split_volume
+        shape, pred_graphs=graph, target_volume=split_volume
     )
     merge_metric.detect_mistakes()
     test1 = merge_metric.site_cnt == 1
     test2 = merge_metric.edge_cnt == 4
-    print('edge_cnt:', merge_metric.edge_cnt)
-    print('site_cnt:', merge_metric.site_cnt)
-    result = 'Pass' if test1 and test2 else 'Fail'
+    print("edge_cnt:", merge_metric.edge_cnt)
+    print("site_cnt:", merge_metric.site_cnt)
+    result = "Pass" if test1 and test2 else "Fail"
     return result
 
+
 def make_split(volume, num_split_edges=1):
-    # Create split
+    """creates split"""
     for i in range(5, 5 + num_split_edges - 1):
         volume[i, i, 0] = 0
 
@@ -119,10 +127,10 @@ if __name__ == "__main__":
     complex_merge_result = test_complex_merge(graph, volume)
 
     # Print out results
-    print('   Result of break test:', break_result)
-    print('   Result of simple split test:', simple_split_result)
-    print('   Result of simple complex split test:', complex_split_result)
-    print('')
+    print("   Result of break test:", break_result)
+    print("   Result of simple split test:", simple_split_result)
+    print("   Result of simple complex split test:", complex_split_result)
+    print("")
 
-    print('   Result of simple merge test:', simple_merge_result)
-    print('   Result of complex merge test:', complex_merge_result)
+    print("   Result of simple merge test:", simple_merge_result)
+    print("   Result of complex merge test:", complex_merge_result)
