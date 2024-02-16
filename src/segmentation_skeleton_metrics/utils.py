8a5f148fa6b4f682296e4d153a44bdf58cbf4c24
anna-grim <anna.grim@alleninstitute.org>
2024-02-16 22:04:51 +0000

    doc: added documentation

--- src/segmentation_skeleton_metrics/utils.py
+++ src/segmentation_skeleton_metrics/utils.py
@@ -46,46 +46,27 @@ def listdir(directory, ext=None):
 
 
 def list_paths(directory, ext=None):
+    """
+    Lists all paths within "directory".
+
+    Parameters
+    ----------
+    directory : str
+        Directory to be searched.
+    ext : str, optional
+        If provided, only paths of files with the extension "ext" are
+        returned. The default is None.
+
+    Returns
+    -------
+    list[str]
+        List of all paths within "directory".
+
+    """
     paths = []
     for f in listdir(directory, ext=ext):
         paths.append(os.path.join(directory, f))
     return paths
-    
-
-def mkdir(path):
-    """
-    Makes a directory at "path" if it does not already exist.
-
-    Parameters
-    ----------
-    path : str
-        Path to directory.
-
-    Returns
-    -------
-    None.
-
-    """
-    if not os.path.exists(path):
-        os.mkdir(path)
-
-
-def rmdir(path):
-    """
-    Removes a directory at "path" if it already exist.
-
-    Parameters
-    ----------
-    path : str
-        Path to directory.
-
-    Returns
-    -------
-    None.
-
-    """
-    if os.path.exists(path):
-        shutil.rmtree(path)
 
 
 # --- data structure utils ---
@@ -176,102 +157,6 @@ def read_tensorstore(path):
     return dataset_ts[ts.d["channel"][0]]
 
 
-def read_tensorstore(arr, xyz, shape, from_center=True):
-    chunk = get_chunk(arr, xyz, shape, from_center=from_center)
-    return chunk.read().result()
-
-
-def get_chunk(arr, xyz, shape, from_center=True):
-    start, end = get_start_end(xyz, shape, from_center=from_center)
-    return deepcopy(
-        arr[start[0]: end[0], start[1]: end[1], start[2]: end[2]]
-    )
-
-
-def read_img(path, filetype):
-    if filetype == "tensorstore":
-        return read_tensorstore(path)
-    elif filetype == "n5":
-        return read_n5(path)
-    else:
-        return read_tif(path)
-
-
-def read_n5(path):
-    """
-    Reads n5 file at "path".
-
-    Parameters
-    ----------
-    path : str
-        Path to n5.
-
-    Returns
-    -------
-    np.array
-        Image volume.
-    """
-    return zarr.open(zarr.N5FSStore(path), "r").volume
-
-
-def read_tif(path):
-    """
-    Reads tif file at "path".
-
-    Parameters
-    ----------
-    path : str
-        Path to tif.
-
-    Returns
-    -------
-    np.array
-        Image volume.
-    """
-    return imread(path)
-
-
-def write_txt(path, contents):
-    """
-    Writes "contents" to a .txt file at "path".
-
-    Parameters
-    ----------
-    path : str
-        Path that .txt file is written to.
-    contents : list[str]
-        Contents to be written to .txt file.
-
-    Returns
-    -------
-    None
-
-    """
-    with open(path, "w") as file:
-        for line in contents:
-            file.write(line + "\n")
-
-
-def write_json(path, contents):
-    """
-    Writes "contents" to a .json file at "path".
-
-    Parameters
-    ----------
-    path : str
-        Path that .txt file is written to.
-    contents : dict
-        Contents to be written to .txt file.
-
-    Returns
-    -------
-    None
-
-    """
-    with open(path, "w") as f:
-        json.dump(contents, f)
-
-        
 # -- miscellaneous --
 def to_world(xyz, anisotropy):
     """
@@ -295,6 +180,24 @@ def to_world(xyz, anisotropy):
 
 
 def time_writer(t, unit="seconds"):
+    """
+    Converts runtime "t" to its proper unit.
+
+    Parameters
+    ----------
+    t : float
+        Runtime to be converted.
+    unit : str, optional
+        Unit of "t".
+
+    Returns
+    -------
+    t : float
+        Converted runtime.
+    unit : str
+        Unit of "t"
+
+    """
     assert unit in ["seconds", "minutes", "hours"]
     upd_unit = {"seconds": "minutes", "minutes": "hours"}
     if t < 60 or unit == "hours":
