bug: erl computation

src/segmentation_skeleton_metrics/graph_utils.py
@@ -9,6 +9,7 @@ Created on Wed Aug 15 12:00:00 2023
 from random import sample
 
 import networkx as nx
+import numpy as np
 from scipy.spatial.distance import euclidean as dist
 
 
@@ -173,8 +174,8 @@ def compute_run_lengths(graph):
 
     Returns
     -------
-    run_lengths : list
-        List of run lengths of each connected component in "graph".
+    run_lengths : numpy.ndarray
+        Array containing run lengths of each connected component in "graph".
 
     """
     run_lengths = []
@@ -184,7 +185,7 @@ def compute_run_lengths(graph):
             run_lengths.append(compute_path_length(subgraph))
     else:
         run_lengths.append(0)
-    return run_lengths
+    return np.array(run_lengths)
 
 
 def compute_path_length(graph):

src/segmentation_skeleton_metrics/skeleton_metric.py
@@ -576,11 +576,38 @@ class SkeletonMetric:
         for swc_id in self.target_graphs.keys():
             pred_graph = self.pred_graphs[swc_id]
             target_graph = self.target_graphs[swc_id]
-            path_lengths = gutils.compute_run_lengths(pred_graph)
-            path_length = gutils.compute_path_length(target_graph)
-            self.erl[swc_id] = np.mean(path_lengths)
-            self.normalized_erl[swc_id] = np.mean(path_lengths) / path_length
 
+            path_length = gutils.compute_path_length(target_graph)
+            path_lengths = gutils.compute_run_lengths(pred_graph)
+            wgts = path_lengths / max(np.sum(path_lengths), 1)
+
+            self.erl[swc_id] = np.sum(wgts * path_lengths)
+            self.normalized_erl[swc_id] = self.erl[swc_id] / path_length
+
+    def list_metrics(self):
+        """
+        Lists metrics that are computed by this module.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        metrics : list[str]
+            List of metrics computed by this module.
+
+        """
+        metrics = [
+            "# splits",
+            "# merges",
+            "% omit edges",
+            "% merged edges",
+            "edge accuracy",
+            "erl",
+            "normalized erl",
+        ]
+        return metrics
 
 # -- utils --
 def is_split(a, b):