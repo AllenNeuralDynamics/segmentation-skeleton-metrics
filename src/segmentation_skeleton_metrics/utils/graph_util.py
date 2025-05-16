772e0c611194e9c63e8e8f29817364a53d63d308
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2025-05-16 09:43:09 -0700

    removed try-except

--- src/segmentation_skeleton_metrics/utils/graph_util.py
+++ src/segmentation_skeleton_metrics/utils/graph_util.py
@@ -146,13 +146,10 @@ class GraphBuilder:
 
         """
         # Initialize graph
-        try:
-            graph = SkeletonGraph(anisotropy=self.anisotropy)
-            graph.init_voxels(swc_dict["voxel"])
-            graph.set_filename(swc_dict["swc_id"] + ".swc")
-            graph.set_nodes(len(swc_dict["id"]))
-        except:
-            print("Failed at Line 155 -", swc_dict["swc_id"] + ".swc")
+        graph = SkeletonGraph(anisotropy=self.anisotropy)
+        graph.init_voxels(swc_dict["voxel"])
+        graph.set_filename(swc_dict["swc_id"] + ".swc")
+        graph.set_nodes(len(swc_dict["id"]))
 
         # Build graph
         id_lookup = dict()
