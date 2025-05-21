1caf84b0c1a1b1cb3265d28b8103d61e791458d9
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2025-05-20 21:11:49 -0700

    Update skeleton_metric.py

--- src/segmentation_skeleton_metrics/skeleton_metric.py
+++ src/segmentation_skeleton_metrics/skeleton_metric.py
@@ -530,7 +530,7 @@ class SkeletonMetric:
 
         """
         # Iterate over fragments that intersect with GT skeleton
-        for label in tqdm(self.get_node_labels(key), desc="Merge Search"):
+        for label in self.get_node_labels(key):
             nodes = self.graphs[key].nodes_with_label(label)
             if len(nodes) > 50:
                 for label in self.label_handler.get_class(label):
