490f5521d078251eb724857267de10b2d08b2ca1
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2026-04-17 10:36:45 -0700

    bug: read s3 text

--- src/segmentation_skeleton_metrics/utils/util.py
+++ src/segmentation_skeleton_metrics/utils/util.py
@@ -515,7 +515,7 @@ def read_txt_from_s3(path):
     str
         Contents of txt file.
     """
-    bucket_name, path = (path)
+    bucket_name, path = parse_cloud_path(path)
     s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
     obj = s3.get_object(Bucket=bucket_name, Key=path)
     return obj["Body"].read().decode("utf-8")
