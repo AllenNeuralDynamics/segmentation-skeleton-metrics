d24a5864392204f92dc2f2afde3abb71706280f4
Anna Grim <108307071+anna-grim@users.noreply.github.com>
2026-06-07 17:28:06 -0700

    Refactor save mips (#221)
    * refactor: more memory efficient mips
    
    * bug: tqdm
    
    ---------
    
    Co-authored-by: anna-grim <anna.grim@alleninstitute.org>

--- src/segmentation_skeleton_metrics/evaluate.py
+++ src/segmentation_skeleton_metrics/evaluate.py
@@ -387,7 +387,7 @@ class Evaluator:
         """
         output_dir = os.path.join(self.output_dir, f"{self.prefix}mips")
         util.mkdir(output_dir, delete=True)
-        for key, gt_graph in tqdm(desc="Save MIPs", gt_graphs.items()):
+        for key, gt_graph in tqdm(gt_graphs.items(), desc="Save MIPs"):
             # Save GT mips
             viz.save_mips([gt_graph], output_dir, gt_graph.name)
 
